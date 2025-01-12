a75681a48099e835e98c3b3ec1c5b403de84f829
onthefasttrack <giridhar.ravipati@gmail.com>
2025-01-12 19:10:22 +0000

    more changes

--- graph.py
+++ graph.py
@@ -34,13 +34,14 @@ def create_llm_message(system_prompt):
     
     # Iterate through chat history, and based on the role (user or assistant) tag it as HumanMessage or AIMessage
     for m in msgs:
-        if m["role"] == "user":
-            # Add user messages as HumanMessage
-            resp.append(HumanMessage(content=m["content"]))
-        elif m["role"] == "assistant":
-            # Add assistant messages as AIMessage
-            resp.append(AIMessage(content=m["content"]))
+      if m["role"] == "user":
+        # Add user messages as HumanMessage
+        resp.append(HumanMessage(content=m["content"]))
+      elif m["role"] == "assistant":
+        # Add assistant messages as AIMessage
+        resp.append(AIMessage(content=m["content"]))
     
+    #resp.append(HumanMessage(content=msgs[0]["content"]))
     # Return the formatted message list
     return resp
 
@@ -59,13 +60,34 @@ def ObtainSchema():
     
         # Look at the most recent human message and find relevant table names
         m = msgs[0]["content"]
-        query = """select table_name, column_name, data_type from user_tab_columns where table_name in 
-                   (select table_name from tabnames order by
+        query = """SELECT
+    '# CREATE TABLE "' || atc.owner || '"."' || atc.table_name || '" ' ||
+    '(' ||
+    LISTAGG('"' || atc.column_name || '" ' || atc.data_type ||
+      CASE
+        WHEN atc.data_type IN ('CHAR', 'VARCHAR2', 'NCHAR', 'NVARCHAR2') THEN
+          '(' || atc.data_length || ')'
+        WHEN atc.data_type = 'NUMBER' AND
+            (atc.data_precision IS NOT NULL OR atc.data_scale IS NOT NULL) THEN
+          '(' || COALESCE(atc.data_precision, 38) || ',' ||
+          COALESCE(atc.data_scale, 0) || ')'
+        ELSE NULL
+      END || ' ''' || REPLACE(acc.comments, '''', '''''') || '''', ',') || ')' AS table_definition
+FROM sys.all_tab_columns atc LEFT JOIN sys.all_col_comments acc
+ON atc.owner = acc.owner AND 
+   atc.table_name = acc.table_name AND
+   atc.column_name = acc.column_name
+WHERE atc.owner = 'ADMIN' AND
+      atc.table_name in (select table_name from tabnames order by
                      vector_distance(vec, TO_VECTOR(VECTOR_EMBEDDING(ALL_MINILM_L12_V2 USING :search_text as DATA)), COSINE)
-                      fetch approx first 5 rows only)"""
+                      fetch approx first 5 rows only)
+GROUP BY atc.owner, atc.table_name"""
+        #query = """select table_name from tabnames order by
+        #             vector_distance(vec, TO_VECTOR(VECTOR_EMBEDDING(ALL_MINILM_L12_V2 USING :search_text as DATA)), COSINE)
+        #              fetch approx first 3 rows only"""
         #query = "select table_name, column_name, data_type from user_tab_columns"
         
-        resp = "The database schema is :"
+        resp = "The tables are :"
         for row in cursor.execute(query, search_text=m):
             resp += f"{row} \n"
         print (resp)
@@ -96,17 +118,18 @@ class FirstAgent:
 
     def classifier(self, state: AgentState):
         CLASSIFIER_PROMPT=f"""
-        You are an expert with deep knowledge of inventory management or sales. 
+        You are an expert with deep knowledge of product, supply chain management, sales of products
         Your job is to comprehend the message from the user even if it lacks specific keywords, 
         always maintain a friendly, professional, and helpful tone. 
         If a user greets you, greet them back by mirroring user's tone and verbosity, and offer assitance. 
 
         Based on user query, accurately classify customer requests into one of the following categories based on context 
         and content, even if specific keywords are not used.
-        1. Manufacturing or sales Question: set category as "manufacturing"
+        1. Manufacturing or inventory or sales or suppliers or employees or consumer data: set category as "manufacturing"
         2. Other: set category as "OTHERS"
         """
         llm_messages = create_llm_message(CLASSIFIER_PROMPT)
+        print (f"{llm_messages=}")
         llm_response = self.model.with_structured_output(Category).invoke(llm_messages)
 
         category = llm_response.category
@@ -126,16 +149,19 @@ class FirstAgent:
     
     def ManufacturingAgent(self, state: AgentState):
         schema = ObtainSchema()
-        SQL_PROMPT=f"""
-        You are an expert with deep knowledge of Oracle SQL. 
-        Your job is to create a sql query to answer the question the user posts.
-        The Database schema is {schema}
-        If there is no sql query for the user request, then mark sql_present as false.
+        SQL_PROMPT = f"""Given an input Question, create a syntactically correct Oracle SQL query to run.
+        - Pay attention to using only the column names that you can see in the schema description.
+        - Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+        - Please double check that the SQL query you generate is valid for Oracle Database.
+        - DO NOT write anything else except the Oracle SQL.
+        - If there is no sql query for the user request, then mark sql_present as false.
+        - Only use the tables listed below {schema}
         """
         llm_messages = create_llm_message(SQL_PROMPT)
         llm_response = self.model.with_structured_output(SQL).invoke(llm_messages)
         sql_present = llm_response.sql_present
         sql_query   = llm_response.sql_query
+        results_present = False
         if sql_present:
           connection=oracledb.connect(
             config_dir="/workspaces/sqltranslator/.streamlit",
@@ -145,16 +171,31 @@ class FirstAgent:
             wallet_location="/workspaces/sqltranslator/.streamlit",
             wallet_password=st.secrets['WALLET_PASS'])
           cursor = connection.cursor()
-          print(sql_query)
-          cursor.execute(sql_query.rstrip(';'))
-          while True:
-            row = cursor.fetchone()
-            if row is None:
-              break
-            result = str(row[0])
-            print(row[0])
-          connection.close()
-          return {"responseToUser": f"{result=}, {sql_query=}"}
+          query_to_issue = sql_query.rstrip(';')
+          query = f"""SELECT JSON_ARRAYAGG(JSON_OBJECT(* ABSENT ON NULL RETURNING CLOB) ABSENT ON NULL RETURNING CLOB PRETTY) FROM (
+    {query_to_issue})"""
+          print(f"{query=}")
+          try:
+            cursor.execute(query)
+            while True:
+              row = cursor.fetchone()
+              if row is None:
+                break
+              result = str(row[0])
+              print(row[0])
+            results_present = True
+          except oracledb.Error as e:
+            error_obj, = e.args
+            print("Error Code:", error_obj.code)
+            print("Error Full Code:", error_obj.full_code)
+            print("Error Message:", error_obj.message)
+          finally:
+            connection.close()
+          
+          if results_present:
+            return {"responseToUser": f"{result=}, {sql_query=}"}
+          else:
+            return {"responseToUser": f"Could not generate the results for query {sql_query=}"}
         else:
           return {"responseToUser": f"Could not generate the sql for this question. Please try a different question"}
 
